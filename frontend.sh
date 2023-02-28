b794bb0c00cb57965726abb6a88b2f3ea8ba250a
Vishnu1995-k <vishnupriyachilakapati@gmail.com>
2023-02-28 20:57:21 +0530

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
-unzip  -o /tmp/frontend.zip
+unzip   /tmp/frontend.zip
 /etc/nginx/default.d/roboshop.conf
 
 systemctl enable nginx
